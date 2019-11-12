3fd31db8c6216a86cdeca2869206db6038463e1c
DamlaDemir <dmldemirr@gmail.com>
2019-11-12 23:56:36 +0300

    app vue project created and menu operations started

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <Menu :label="data.TANIM" :nodes="data.MENU"></Menu>
     </div>
     <router-view />
   </div>
 </template>
+<script>
+import Menu from "./components/Menu";
 
+export default {
+  components: {
+    Menu
+  },
+  data () {
+    return {
+      data : []
+      }
+  },
+  created() {
+    debugger;
+   this.data = require("./data/menu.json");
+   console.log(this.data)
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
---
+++ src/components/Menu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="tree-menu">
+    <div>{{ label }}</div>
+    <Menu 
+      v-for="node in nodes" 
+      :key="node.ID"
+      :nodes="node.MENU" 
+      :label="node.TANIM"
+    >
+    </Menu>
+
+  </div>
+</template>
+<script>
+  export default { 
+    props: [ 'label', 'nodes' ],
+    name: 'Menu'
+  }
+</script>
